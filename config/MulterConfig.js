2ef240f22cee3009595ad88723e0cf05cfac9bc6
Azadev <kodingo593@gmail.com>
2024-08-20 12:32:41 +0400

    s

--- config/MulterConfig.js
+++ config/MulterConfig.js
@@ -1,10 +1,11 @@
 const multer = require("multer");
 const path = require("path");
 const { v4: uuidv4 } = require("uuid");
+const diskMountPath = '/var/data';
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, "../public"));
+    cb(null, path.join(diskMountPath, 'public'));
   },
   filename: (req, file, cb) => {
     const ext = path.extname(file.originalname);
