0375c0cff3a156dfd37f0a9afa49b2d3edec8b66
Azadev <kodingo593@gmail.com>
2024-09-13 15:25:48 +0400

    ısdls

--- routes/User.js
+++ routes/User.js
@@ -5,25 +5,21 @@ const jwt = require("jsonwebtoken");
 
 // POST /login
 router.post("/login", async (req, res) => {
-  const { email, password } = req.body; // req.file yerine req.body kullanmalısınız
+  const { email, password } = req.body; 
 
   try {
-    // 1. Kullanıcıyı bul
     const user = await User.findOne({ email });
     if (!user) {
       return res.status(401).json({ message: "Not found user" });
     }
 
-    // 2. Şifreyi kontrol et
     const isMatch = await user.comparePassword(password);
     if (!isMatch) {
       return res.status(402).json({ message: "Invalid email or password" });
     }
 
-    // 3. JWT token oluştur
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
 
-    // 4. Token'i kullanıcıya gönder
     res.status(200).json({
       success: true,
       token,
