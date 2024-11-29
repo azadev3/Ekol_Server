7c471c2b7b87bcbc3b35171755c14cca08f3ea0e
Azadev <kodingo593@gmail.com>
2024-11-29 11:39:40 +0400

    sdlsk

--- routes/RoleRoute.js
+++ routes/RoleRoute.js
@@ -33,7 +33,7 @@ router.post("/create_role", async (req, res) => {
 });
 
 router.get("/create_role", async (req, res) => {
-  const roles = await RoleModel.find().populate("Permission");
+  const roles = await RoleModel.find().populate("permissions");
 
   if (!roles) {
     return res.status(400).json({ msg: "roles is empty" });
