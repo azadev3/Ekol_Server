1b14ced8295997fee9382a5c320d59212fa079c4
Azadev <kodingo593@gmail.com>
2024-11-29 13:58:39 +0400

    dls

--- models/PermissionModel.js
+++ models/PermissionModel.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const PermissionSchema = mongoose.Schema({
   name: { type: String, required: true },
-  key: { type: String, required: true },
+  value: { type: String, required: true },
 });
 
 module.exports = mongoose.model("Permission", PermissionSchema);
