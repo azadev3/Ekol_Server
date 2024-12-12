6e6306477aafd668d29f2e80e17f2c45b0ce95bc
Azadev <kodingo593@gmail.com>
2024-12-12 14:03:21 +0400

    dfl

--- routes/ShowHiddenCarier.js
+++ routes/ShowHiddenCarier.js
@@ -1,35 +1,31 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const ShowHiddenCarierModel = require("../models/ShowHiddenCarierModel");
-const checkUser = require("../middlewares/checkUser");
-const checkPermissions = require("../middlewares/checkPermissions");
+const ShowHiddenCarierModel = require('../models/ShowHiddenCarierModel');
+const checkUser = require('../middlewares/checkUser');
+const checkPermissions = require('../middlewares/checkPermissions');
 
-router.post("/hidden-carier", async (req, res) => {
-  try {
-    const { show } = req.body;
+router.post('/hidden-carier', async (req, res) => {
+ try {
+  const { show } = req.body;
 
-    const saved = await ShowHiddenCarierModel.findOneAndUpdate(
-      {},
-      { showed: show }, 
-      { upsert: true, new: true } 
-    );
+  const saved = await ShowHiddenCarierModel.findOneAndUpdate({}, { showed: show }, { upsert: true, new: true });
 
-    return res.status(200).json({ saved });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: error });
-  }
+  return res.status(200).json({ saved });
+ } catch (error) {
+  console.log(error);
+  res.status(500).json({ error: error });
+ }
 });
 
-router.get("/hidden-carier-front", async (req, res) => {
-  try {
-    const showState = await ShowHiddenCarierModel.findOne();
+router.get('/hidden-carier-front', async (req, res) => {
+ try {
+  const showState = await ShowHiddenCarierModel.findOne();
 
-    return res.status(200).json(showState);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: error });
-  }
+  return res.status(200).json(showState);
+ } catch (error) {
+  console.log(error);
+  res.status(500).json({ error: error });
+ }
 });
 
 module.exports = router;
