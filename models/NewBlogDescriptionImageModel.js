89524372bd8ada70711ed44a843be584abe803f5
Azadev <kodingo593@gmail.com>
2024-12-23 13:53:08 +0400

    Asdlas

--- models/NewBlogDescriptionImageModel.js
+++ models/NewBlogDescriptionImageModel.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const NewBlogDescriptionImageSchema = mongoose.Schema({
   images: { type: [String], required: true },
-  selected_blog: { type: String, required: true },
+  selected_new_blog: { type: String, required: true },
 });
 
 const NewBlogDescriptionImageModel = mongoose.model("newblogimage", NewBlogDescriptionImageSchema);
