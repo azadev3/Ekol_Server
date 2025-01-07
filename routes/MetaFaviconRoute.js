439dbf010ab94d3c5ffa309768d2f6fc645e87d4
Azadev <kodingo593@gmail.com>
2025-01-07 15:58:06 +0400

    sdfkds

--- routes/MetaFaviconRoute.js
+++ routes/MetaFaviconRoute.js
@@ -8,6 +8,7 @@ router.post('/upload-favicon', upload.single('favicon'), async (req, res) => {
     if (!req.file) {
       return res.status(400).json({ error: 'Please upload a file.' });
     }
+
     const imageFile = req.file ? `/public/${req.file.filename}` : '';
 
     const existingFavicon = await MetaFaviconModel.findOne();
@@ -15,13 +16,13 @@ router.post('/upload-favicon', upload.single('favicon'), async (req, res) => {
     if (existingFavicon) {
       existingFavicon.favicon = imageFile;
       const updatedFavicon = await existingFavicon.save();
-      return res.status(200).json(updatedFavicon);
+      return res.status(200).json(updatedFavicon); 
     } else {
       const createData = new MetaFaviconModel({
         favicon: imageFile,
       });
       const savedData = await createData.save();
-      return res.status(200).json(savedData);
+      return res.status(200).json(savedData); 
     }
   } catch (error) {
     console.error('Upload Favicon Error:', error.message);
@@ -30,12 +31,18 @@ router.post('/upload-favicon', upload.single('favicon'), async (req, res) => {
 });
 
 router.get('/get-favicon', async (req, res) => {
-  const data = MetaFaviconModel.find();
-  if (!data) {
-    return res.status(404).json({ message: 'not found favicon' });
-  }
+  try {
+    const faviconData = await MetaFaviconModel.findOne();
+    
+    if (!faviconData || !faviconData.favicon) {
+      return res.status(404).json({ message: 'Favicon not found.' });
+    }
 
-  return res.status(200).json(data);
+    return res.status(200).json(faviconData);
+  } catch (error) {
+    console.error('Get Favicon Error:', error.message);
+    return res.status(500).json({ error: error.message });
+  }
 });
 
 module.exports = router;
