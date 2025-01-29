bac919b4f0aeb2f7bc7cbcd39385a1c1bc5faba1
Azadev <kodingo593@gmail.com>
2025-01-29 10:35:40 +0400

    sdfksdk

--- models/ServicesPageModel.js
+++ models/ServicesPageModel.js
@@ -1,27 +1,40 @@
 const mongoose = require('mongoose');
 
+// Tarihi "DD.MM.YYYY" formatında döndüren fonksiyon
+const formatDate = () => {
+  const now = new Date();
+  const day = String(now.getDate()).padStart(2, '0');
+  const month = String(now.getMonth() + 1).padStart(2, '0'); // Aylar 0'dan başlar, +1 eklenmeli
+  const year = now.getFullYear();
+  return `${day}.${month}.${year}`;
+};
+
 const ServicesPageSchema = mongoose.Schema({
- title: {
-  az: { type: String, required: false, default: '' },
-  en: { type: String, required: false, default: '' },
-  ru: { type: String, required: false, default: '' },
- },
- slogan: {
-  az: { type: String, required: false, default: '' },
-  en: { type: String, required: false, default: '' },
-  ru: { type: String, required: false, default: '' },
- },
- description: {
-  az: { type: String, required: false, default: '' },
-  en: { type: String, required: false, default: '' },
-  ru: { type: String, required: false, default: '' },
- },
- image: { type: String, required: false, default: '' },
- statusActive: {
-  type: Boolean,
-  default: true,
-  required: false,
- },
+  title: {
+    az: { type: String, required: false, default: '' },
+    en: { type: String, required: false, default: '' },
+    ru: { type: String, required: false, default: '' },
+  },
+  slogan: {
+    az: { type: String, required: false, default: '' },
+    en: { type: String, required: false, default: '' },
+    ru: { type: String, required: false, default: '' },
+  },
+  description: {
+    az: { type: String, required: false, default: '' },
+    en: { type: String, required: false, default: '' },
+    ru: { type: String, required: false, default: '' },
+  },
+  image: { type: String, required: false, default: '' },
+  statusActive: {
+    type: Boolean,
+    default: true,
+    required: false,
+  },
+  created_at: {
+    type: String,
+    default: formatDate, 
+  },
 });
 
 const ServicesPageModel = mongoose.model('servicespagemodel', ServicesPageSchema);
--- routes/ServicesPage.js
+++ routes/ServicesPage.js
@@ -1,17 +1,17 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const ServicesPage = require("../models/ServicesPageModel");
-const path = require("path");
-const { v4: uuidv4 } = require("uuid");
-const { uploadConfig, useSharp } = require("../config/MulterC");
-const mountPath = require("../config/mountPath");
-const checkUser = require("../middlewares/checkUser");
-const checkPermission = require("../middlewares/checkPermissions"); 
+const ServicesPage = require('../models/ServicesPageModel');
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+const { uploadConfig, useSharp } = require('../config/MulterC');
+const mountPath = require('../config/mountPath');
+const checkUser = require('../middlewares/checkUser');
+const checkPermission = require('../middlewares/checkPermissions');
 
-router.post("/servicespage", checkUser, checkPermission("create_xidmetler_daxili"), uploadConfig.single("imgback"), async (req, res) => {
+router.post('/servicespage', checkUser, checkPermission('create_xidmetler_daxili'), uploadConfig.single('imgback'), async (req, res) => {
   try {
     // Img
-    let imageFile = "";
+    let imageFile = '';
 
     if (req.file) {
       const imgFileName = `${uuidv4()}-${Date.now()}.webp`;
@@ -40,7 +40,6 @@ router.post("/servicespage", checkUser, checkPermission("create_xidmetler_daxili
       },
       image: imageFile,
       statusActive: req.body.statusActive || true,
-
     });
 
     const savedData = await createData.save();
@@ -51,11 +50,11 @@ router.post("/servicespage", checkUser, checkPermission("create_xidmetler_daxili
   }
 });
 
-router.get("/servicespage", checkUser, checkPermission("list_xidmetler_daxili"), async (req, res) => {
+router.get('/servicespage', checkUser, checkPermission('list_xidmetler_daxili'), async (req, res) => {
   try {
     const datas = await ServicesPage.find();
     if (!datas || datas.length === 0) {
-      return res.status(404).json({ message: "No data found" });
+      return res.status(404).json({ message: 'No data found' });
     }
     return res.status(200).json(datas);
   } catch (error) {
@@ -63,19 +62,19 @@ router.get("/servicespage", checkUser, checkPermission("list_xidmetler_daxili"),
   }
 });
 
-router.get("/servicespage/:editid", async (req, res) => {
+router.get('/servicespage/:editid', async (req, res) => {
   try {
     const { editid } = req.params;
 
     const datasForId = await ServicesPage.findById(editid).lean().exec();
 
     if (!datasForId) {
-      return res.status(404).json({ error: "not found editid" });
+      return res.status(404).json({ error: 'not found editid' });
     }
 
     return res.status(200).json(datasForId);
   } catch (error) {
-    console.error("Error fetching data:", error);
+    console.error('Error fetching data:', error);
     return res.status(500).json({ error: error.message });
   }
 });
@@ -130,122 +129,115 @@ router.get("/servicespage/:editid", async (req, res) => {
 //   }
 // });
 
-router.put("/servicespage/:editid", checkUser, checkPermission("update_xidmetler_daxili"), uploadConfig.single("imgback"), async (req, res) => {
-  try {
-    const { editid } = req.params;
-    const {
-      title_az,
-      title_en,
-      title_ru,
-      description_az,
-      description_en,
-      description_ru,
-      slogan_az,
-      slogan_en,
-      slogan_ru,
-    } = req.body;
+router.put(
+  '/servicespage/:editid',
+  checkUser,
+  checkPermission('update_xidmetler_daxili'),
+  uploadConfig.single('imgback'),
+  async (req, res) => {
+    try {
+      const { editid } = req.params;
+      const { title_az, title_en, title_ru, description_az, description_en, description_ru, slogan_az, slogan_en, slogan_ru } = req.body;
 
-    const existingServicePage = await ServicesPage.findById(editid).exec();
-    if (!existingServicePage) {
-      return res.status(404).json({ error: "Service page not found" });
+      const existingServicePage = await ServicesPage.findById(editid).exec();
+      if (!existingServicePage) {
+        return res.status(404).json({ error: 'Service page not found' });
+      }
+
+      const updatedData = {};
+
+      if (title_az || title_en || title_ru) {
+        updatedData.title = {
+          az: title_az || existingServicePage.title.az,
+          en: title_en || existingServicePage.title.en,
+          ru: title_ru || existingServicePage.title.ru,
+        };
+      }
+
+      if (description_az || description_en || description_ru) {
+        updatedData.description = {
+          az: description_az || existingServicePage.description.az,
+          en: description_en || existingServicePage.description.en,
+          ru: description_ru || existingServicePage.description.ru,
+        };
+      }
+
+      if (slogan_az || slogan_en || slogan_ru) {
+        updatedData.slogan = {
+          az: slogan_az || existingServicePage.slogan.az,
+          en: slogan_en || existingServicePage.slogan.en,
+          ru: slogan_ru || existingServicePage.slogan.ru,
+        };
+      }
+
+      if (req.file) {
+        const imgFileName = `${uuidv4()}-${Date.now()}.webp`;
+        const imgOutputPath = path.join(mountPath, imgFileName);
+        await useSharp(req.file.buffer, imgOutputPath);
+
+        updatedData.image = `/public/${imgFileName}`;
+      } else {
+        updatedData.image = existingServicePage.image;
+      }
+
+      if (Object.keys(updatedData).length === 0) {
+        return res.status(200).json(existingServicePage);
+      }
+
+      const updatedServicePage = await ServicesPage.findByIdAndUpdate(editid, { $set: updatedData }, { new: true }).lean().exec();
+
+      return res.status(200).json(updatedServicePage);
+    } catch (error) {
+      console.error('Error updating data:', error);
+      return res.status(500).json({ error: error.message });
     }
+  },
+);
 
-    const updatedData = {};
-
-    if (title_az || title_en || title_ru) {
-      updatedData.title = {
-        az: title_az || existingServicePage.title.az,
-        en: title_en || existingServicePage.title.en,
-        ru: title_ru || existingServicePage.title.ru,
-      };
-    }
-
-    if (description_az || description_en || description_ru) {
-      updatedData.description = {
-        az: description_az || existingServicePage.description.az,
-        en: description_en || existingServicePage.description.en,
-        ru: description_ru || existingServicePage.description.ru,
-      };
-    }
-
-    if (slogan_az || slogan_en || slogan_ru) {
-      updatedData.slogan = {
-        az: slogan_az || existingServicePage.slogan.az,
-        en: slogan_en || existingServicePage.slogan.en,
-        ru: slogan_ru || existingServicePage.slogan.ru,
-      };
-    }
-
-    if (req.file) {
-      const imgFileName = `${uuidv4()}-${Date.now()}.webp`;
-      const imgOutputPath = path.join(mountPath, imgFileName);
-      await useSharp(req.file.buffer, imgOutputPath);
-
-      updatedData.image = `/public/${imgFileName}`;
-    } else {
-      updatedData.image = existingServicePage.image;
-    }
-
-    if (Object.keys(updatedData).length === 0) {
-      return res.status(200).json(existingServicePage);
-    }
-
-    const updatedServicePage = await ServicesPage.findByIdAndUpdate(editid, { $set: updatedData }, { new: true })
-      .lean()
-      .exec();
-
-    return res.status(200).json(updatedServicePage);
-  } catch (error) {
-    console.error("Error updating data:", error);
-    return res.status(500).json({ error: error.message });
-  }
-});
-
-router.delete("/servicespage/:deleteid", checkUser, checkPermission("delete_xidmetler_daxili"), async (req, res) => {
+router.delete('/servicespage/:deleteid', checkUser, checkPermission('delete_xidmetler_daxili'), async (req, res) => {
   try {
     const { deleteid } = req.params;
     const deleteData = await ServicesPage.findByIdAndDelete(deleteid);
 
     if (!deleteData) {
-      return res.status(404).json({ message: "dont delete data or not found data or another error" });
+      return res.status(404).json({ message: 'dont delete data or not found data or another error' });
     }
 
-    return res.status(200).json({ message: "successfully deleted data" });
+    return res.status(200).json({ message: 'successfully deleted data' });
   } catch (error) {}
 });
 
-
-router.put("/servicespage/status/:id", async (req, res) => {
+router.put('/servicespage/status/:id', async (req, res) => {
   try {
     const { id } = req.params;
     const { statusActive } = req.body;
 
-    if (typeof statusActive !== "boolean") {
-      return res.status(400).json({ error: "Status must be a boolean value" });
+    if (typeof statusActive !== 'boolean') {
+      return res.status(400).json({ error: 'Status must be a boolean value' });
     }
 
     const updatedPurch = await ServicesPage.findByIdAndUpdate(id, { statusActive: statusActive }, { new: true }).lean().exec();
 
     if (!updatedPurch) {
-      return res.status(404).json({ error: " not found" });
+      return res.status(404).json({ error: ' not found' });
     }
 
     return res.status(200).json(updatedPurch);
   } catch (error) {
-    console.error("Error updating status:", error);
+    console.error('Error updating status:', error);
     return res.status(500).json({ error: error.message });
   }
 });
 
 // for front
-router.get("/servicespagefront", async (req, res) => {
+router.get('/servicespagefront', async (req, res) => {
   try {
-    const acceptLanguage = req.headers["accept-language"];
-    const preferredLanguage = acceptLanguage.split(",")[0].split(";")[0];
+    const acceptLanguage = req.headers['accept-language'];
+    const preferredLanguage = acceptLanguage.split(',')[0].split(';')[0];
 
     const datas = await ServicesPage.find({ statusActive: true });
     if (!datas || datas.length === 0) {
-      return res.status(404).json({ message: "No data found" });
+      return res.status(404).json({ message: 'No data found' });
     }
 
     const filteredData = datas.map((data) => ({
@@ -254,6 +246,7 @@ router.get("/servicespagefront", async (req, res) => {
       description: data.description[preferredLanguage],
       slogan: data.slogan[preferredLanguage],
       image: data.image,
+      created_at: data.created_at,
     }));
 
     return res.status(200).json(filteredData);
