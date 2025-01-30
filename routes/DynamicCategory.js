3c74b9f72ad6868b30ea198483ff0d733d086aae
Azadev <kodingo593@gmail.com>
2025-01-30 09:00:09 +0400

    sdfksdkfds

--- routes/DynamicCategory.js
+++ routes/DynamicCategory.js
@@ -4,13 +4,23 @@ const multer = require("multer");
 const CategoryModel = require("../models/DynamicCategoryModel");
 const upload = require("../config/MulterConfig");
 
-router.post("/dynamic-category", upload.fields([
-    { name: "product[][pdf][az]" },
-    { name: "product[][pdf][en]" },
-    { name: "product[][pdf][ru]" }
-]), async (req, res) => {
+
+router.post("/dynamic-category", upload.any(), async (req, res) => {
     try {
         const { category_title, product } = req.body;
+
+        const processedProducts = product.map((p, index) => ({
+            title: {
+                az: p.title.az,
+                en: p.title.en,
+                ru: p.title.ru
+            },
+            pdf: {
+                az: req.files.find(file => file.fieldname === `product_${index}_pdf_az`)?.path || "",
+                en: req.files.find(file => file.fieldname === `product_${index}_pdf_en`)?.path || "",
+                ru: req.files.find(file => file.fieldname === `product_${index}_pdf_ru`)?.path || "",
+            }
+        }));
 
         const newCategory = new CategoryModel({
             category_title: {
@@ -18,26 +28,17 @@ router.post("/dynamic-category", upload.fields([
                 en: category_title.en,
                 ru: category_title.ru
             },
-            product: product.map((p, index) => ({
-                title: {
-                    az: p.title.az,
-                    en: p.title.en,
-                    ru: p.title.ru,
-                },
-                pdf: {
-                    az: req.files?.[`product[${index}][pdf][az]`] ? req.files[`product[${index}][pdf][az]`][0].path : "",
-                    en: req.files?.[`product[${index}][pdf][en]`] ? req.files[`product[${index}][pdf][en]`][0].path : "",
-                    ru: req.files?.[`product[${index}][pdf][ru]`] ? req.files[`product[${index}][pdf][ru]`][0].path : "",
-                }
-            }))
+            product: processedProducts
         });
 
         await newCategory.save();
-        res.status(201).json({ message: "Category successfully created" });
+
+        res.status(201).json({ message: "Successfully added Dynamic Category", data: newCategory });
     } catch (error) {
         console.error(error);
-        res.status(500).json({ error: "Server error" });
+        res.status(500).json({ message: "Server error!" });
     }
 });
+
 
 module.exports = router;
