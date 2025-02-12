53a7c4b7d501d0e4e3bd4b47f498e20512c7b86e
Azadev <kodingo593@gmail.com>
2025-02-12 15:07:03 +0400

    sdfksdk

--- routes/AddIconFooter.js
+++ routes/AddIconFooter.js
@@ -3,30 +3,28 @@ const router = express.Router();
 const upload = require('../config/MulterConfig');
 const AddIconFooterModel = require('../models/AddIconFooterModel');
 
-router.post('/add-icon-footer', upload.single("icon"), async (req, res) => {
-    try {
-        const [title, color, url] = req.body;
-        const imageFile = req.body.file;
+router.post('/add-icon-footer', upload.single('icon'), async (req, res) => {
+  try {
+    const imageFile = req.body.file;
 
-        if (!imageFile) {
-            return res.status(400).json({ error: 'not found req body file' });
-        }
-
-        const newModel = AddIconFooterModel({
-            title: title,
-            color: color,
-            url: url,
-            icon: imageFile,
-        });
-
-        const save = await newModel.save();
-        
-        return res.status(200).json(save);
-
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({ error: error.message })
+    if (!imageFile) {
+      return res.status(400).json({ error: 'not found req body file' });
     }
+
+    const newModel = AddIconFooterModel({
+      title: req.body.title,
+      color: req.body.color,
+      url: req.body.url,
+      icon: imageFile,
+    });
+
+    const save = await newModel.save();
+
+    return res.status(200).json(save);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: error.message });
+  }
 });
 
 module.exports = router;
